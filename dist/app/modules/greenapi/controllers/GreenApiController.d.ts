ec9c94f8713f3b053b391c492343c5addaba6953
nmolchanov <nmolchanov@samokat.ru>
2024-07-24 21:12:01 +0300

    add dist to gitignore

--- dist/app/modules/greenapi/controllers/GreenApiController.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class GreenApiController {
-    root(): {};
-    getSettings(idInstance: string, apiTokenInstance: string): Promise<any>;
-    getStateInstance(idInstance: string, apiTokenInstance: string): Promise<any>;
-    sendMessage(body: any): Promise<any>;
-    sendFileByUrl(body: any): Promise<any>;
-}
